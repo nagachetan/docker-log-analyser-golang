d342ce2f1dfa4df97d894e1c77500c2db7896482
nagachetan.km <nagachetan.km@gmail.com>
2019-10-27 17:59:19 +0530

    Added code to read json file and display in tabulated form

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # docker-log-analyser-golang
 This project is to tabulate the big set of logs coming out of docker containers or k8s pods. It will help to debug/analyse the logs
+
+1. Desired log level can be input so that those log levels only will be filtered out and displayed in tabulated form.
+2. Fonts of each column can be set with desired color
+3. Currently 6 fields are added in the program this can be extended by adding more fields in the structure and extending the display of the table.
+4. if it needs to be in html format it can be enabled with an option RenderHTML and it will generate the result into the html file.
+
---
+++ loggerTable.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/jedib0t/go-pretty/table"
+	"github.com/jedib0t/go-pretty/text"
+	"os"
+)
+
+type LogDetails struct {
+	Loglevel   string `json:"level"`
+	TimeStamp  string `json:"ts"`
+	Msg        string `json:"msg"`
+	Caller     string `json:"caller"`
+	DevInfo    string `json:"deviceInfo"`
+	StackTrace string `json:"stacktrace"`
+}
+
+type LgLevel struct {
+	err   bool
+	info  bool
+	warn  bool
+	debug bool
+}
+
+func readJSONFile(fileName string, filePath string, level LgLevel) {
+	// File Reader
+	file, err := os.Open(filePath + fileName)
+
+	if err != nil {
+		fmt.Print("failed opening file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtLines []string
+
+	for scanner.Scan() {
+		txtLines = append(txtLines, scanner.Text())
+	}
+
+	_ = file.Close()
+
+	// Initialize table format
+	t := table.NewWriter()
+	// you can also instantiate the object directly
+	tTemp := table.Table{}
+	tTemp.Render() // just to avoid the compile error of not using the object
+	// HEADER fields to be added here
+	t.AppendHeader(table.Row{"Level", "ts", "msg", "comment", "deviceInfo", "stacktrace"})
+
+	t.SetAutoIndex(true)
+	for _, eachline := range txtLines {
+		var ld LogDetails
+		_ = json.Unmarshal([]byte(eachline), &ld)
+		
+		if (level.err == true && "error" == ld.Loglevel) || (level.debug == true && "debug" == ld.Loglevel) || (level.info == true && "info" == ld.Loglevel) || (level.warn == true && "warn" == ld.Loglevel) {
+
+			if ld.DevInfo != "" && ld.StackTrace == "" {
+				t.AppendRow(table.Row{ld.Loglevel, ld.TimeStamp, ld.Msg, ld.Caller, string(ld.DevInfo)})
+			}
+			if ld.StackTrace != "" && ld.DevInfo == "" {
+				t.AppendRow(table.Row{ld.Loglevel, ld.TimeStamp, ld.Msg, ld.Caller, "", string(ld.StackTrace)})
+			}
+			if ld.StackTrace != "" && ld.DevInfo != "" {
+				t.AppendRow(table.Row{ld.Loglevel, ld.TimeStamp, ld.Msg, ld.Caller, string(ld.DevInfo), string(ld.StackTrace)})
+			}
+			if ld.DevInfo == "" && ld.StackTrace == "" {
+				t.AppendRow(table.Row{ld.Loglevel, ld.TimeStamp, ld.Msg, ld.Caller})
+			}
+		}
+	}
+	t.SetStyle(table.StyleBold)
+	colorBOnW := text.Colors{text.BgWhite, text.FgBlack}
+	t.SetColorsHeader([]text.Colors{colorBOnW, colorBOnW, colorBOnW, colorBOnW, colorBOnW, colorBOnW})
+	t.SetColors([]text.Colors{{text.FgGreen}, {text.FgHiBlue}, {text.FgHiWhite}, {text.FgHiYellow}, {text.FgCyan}, {text.FgHiMagenta}})
+	t.SetColorsFooter([]text.Colors{{}, {}, colorBOnW, colorBOnW})
+	t.SetAllowedColumnLengths([]int{0, 6, 30, 20, 30, 30})
+	t.Style().Options.DrawBorder = true
+	t.Style().Options.SeparateRows = true
+	t.SetCaption(fileName)
+
+	fmt.Println(t.Render())
+	// To render into html enable this option
+	//fmt.Println(t.RenderHTML())
+
+}
+
+func main() {
+	fmt.Println("Starting-Analysig-JSON-file", )
+	fileName := "sample.json"
+	filePath := "/home/demo/"
+	level := LgLevel{
+		err:   true,
+		info:  true,
+		warn:  false,
+		debug: true,
+	}
+	readJSONFile(fileName, filePath, level)
+}
+
