f644f1f61524619aff1a63ae8f343021e5225635
nagachetan.km <nagachetan.km@gmail.com>
2019-12-15 23:03:32 +0530

    updated fix

--- loggerTable.go
+++ loggerTable.go
@@ -58,16 +58,16 @@ func readJSONFile(fileName string, filePath string, level LgLevel, pattern strin
 		
 		if (level.err == true && "error" == ld.Loglevel) || (level.debug == true && "debug" == ld.Loglevel) || (level.info == true && "info" == ld.Loglevel) || (level.warn == true && "warn" == ld.Loglevel) {
 
-			if ld.DevInfo != "" && ld.StackTrace == "" && strings.Contains(ld.Msg.pattern) {
+			if ld.DevInfo != "" && ld.StackTrace == "" && strings.Contains(ld.Msg, pattern) {
 				t.AppendRow(table.Row{ld.Loglevel, ld.TimeStamp, ld.Msg, ld.Caller, string(ld.DevInfo)})
 			}
-			if ld.StackTrace != "" && ld.DevInfo == "" && strings.Contains(ld.Msg.pattern) {
+			if ld.StackTrace != "" && ld.DevInfo == "" && strings.Contains(ld.Msg, pattern) {
 				t.AppendRow(table.Row{ld.Loglevel, ld.TimeStamp, ld.Msg, ld.Caller, "", string(ld.StackTrace)})
 			}
-			if ld.StackTrace != "" && ld.DevInfo != "" && strings.Contains(ld.Msg.pattern) {
+			if ld.StackTrace != "" && ld.DevInfo != "" && strings.Contains(ld.Msg, pattern) {
 				t.AppendRow(table.Row{ld.Loglevel, ld.TimeStamp, ld.Msg, ld.Caller, string(ld.DevInfo), string(ld.StackTrace)})
 			}
-			if ld.DevInfo == "" && ld.StackTrace == "" && strings.Contains(ld.Msg.pattern) {
+			if ld.DevInfo == "" && ld.StackTrace == "" && strings.Contains(ld.Msg, pattern) {
 				t.AppendRow(table.Row{ld.Loglevel, ld.TimeStamp, ld.Msg, ld.Caller})
 			}
 		}
